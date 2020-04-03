1a72e22791f58dc2d349d451a9e7c8fb1475ce5a
Abrarul Hoque <jubairctg99@gmail.com>
2020-04-04 01:33:07 +0600

    node-mongo problems finished

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require ('cors');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
-
+require('dotenv').config()
 
 const app = express();
 
@@ -10,12 +10,29 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
-const user = "dbUser";
-const password = "@abc123@";
-const uri = "mongodb+srv://dbUser:@abc123@@cluster0-tdttw.mongodb.net/test?retryWrites=true&w=majority";
+const uri = process.env.DB_PATH;
 
-const client = new MongoClient(uri, { useNewUrlParser: true });
-const users = ["Abrar", "Nasim", "Lubs", "Shadin", "Sakib"]
+
+let client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const users = ["Abrar", "Nasim", "Lubs", "Shadin", "Towhida"]
+
+//Get data
+app.get('/products', (req, res) =>{
+    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+        const collection = client.db("onlineStore").collection("products");
+        collection.find().limit(20).toArray((err, documents) => {
+            if (err){
+                console.log(err);
+                res.status(500).send({message:err})
+            }
+            else{
+                res.send(documents);
+            }
+        });
+        //client.close();
+      });
+});
 
 
 app.get('/users/:id', (req, res)=> {
@@ -27,22 +44,21 @@ app.get('/users/:id', (req, res)=> {
 //Post
 app.post('/addProduct', (req, res) => {
     const product = req.body;
-    console.log(product);
+    
     client.connect(err => {
         const collection = client.db("onlineStore").collection("products");
         collection.insertOne(product, (err, result) => {
             if (err){
-                console.log(err)
-                res.status(500).send({message:err});
+                res.status(500).send({message:err})
             }
             else{
                 res.send(result.ops[0]);
             }
         });
-        client.close();
+        //client.close();
       });
 });
 
 
-
+const port = process.env.PORT || 4100;
 app.listen(3100, () => console.log('Listening to port 3100'))
